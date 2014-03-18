8294ba2007f040981a6b9ee7e8568091c0ddd922
pipe <pipe@como-HP-dx2480-MT-FN870PA.(none)>
2014-03-18 09:13:06 +0530

    changes inthe issue module

---
+++ src/PS/Bundle/BalanceBudgetBundle/Admin/IssueAdmin.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace PS\Bundle\BalanceBudgetBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class IssueAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+//    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+//    {
+//        $datagridMapper
+//           // ->add('id')
+//            ->add('name')
+//            ->add('type')    
+//            ->add('title')
+//            ->add('lead')
+//            ->add('description')
+//    
+//            ->add('is_activate')
+//                
+//            //->add('created_at')
+//        ;
+//    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+           // ->add('id')
+            ->add('name')
+            ->add('type')    
+            ->add('title')
+            ->add('lead')
+            ->add('description')
+            ->add('is_activate')
+           // ->add('created_at')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+           // ->add('id')
+            ->add('name')
+            ->add('type','entity',array('class' => 'PSBalanceBudgetBundle:IssueType','property' => 'name'))    
+            ->add('title')
+            ->add('lead')
+            ->add('description')
+          
+            ->add('is_activate')
+           // ->add('created_at')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('name')
+            ->add('type')    
+            ->add('title')
+            ->add('lead')
+            ->add('description')
+            ->add('is_activate')
+            ->add('created_at')
+        ;
+    }
+}

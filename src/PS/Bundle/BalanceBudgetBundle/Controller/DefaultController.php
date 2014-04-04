ec92a57f22c5794bb4402a29f40a5ed5b0ccf1bb
Pushparaj <pushparaj@comogroupap.com>
2014-04-04 14:46:19 +0530

    changes from develop

--- src/PS/Bundle/BalanceBudgetBundle/Controller/DefaultController.php
+++ src/PS/Bundle/BalanceBudgetBundle/Controller/DefaultController.php
@@ -72,7 +72,7 @@ class DefaultController extends Controller
 
                 if(!isset($sessionId) || !$plannerPostCode)
                 {
-                    $session->set('id', $session->getId());
+                   // $session->set('id', $session->getId());
                     $plannerPostCode->setSessionId($sessionId);
                     $em->persist($plannerPostCode);
                     $em->flush();

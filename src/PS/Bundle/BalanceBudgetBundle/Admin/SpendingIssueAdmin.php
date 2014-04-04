22f09aa5497f8ebe4aff987bc33b6187a460bb89
Srinivasan Kumar <srini@comogroupap.com>
2014-04-04 13:55:03 +0530

    Spending admin classes added

---
+++ src/PS/Bundle/BalanceBudgetBundle/Admin/SpendingIssueAdmin.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace PS\Bundle\BalanceBudgetBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class SpendingIssueAdmin extends Admin
+{
+    protected $baseRoutePattern = '/spending/issue';
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+//            ->add('id')
+            ->add('name')
+            ->add('section')
+            ->add('description')
+            ->add('option_values')
+            ->add('created_at')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+//            ->add('id')
+            ->add('name')
+            ->add('description')
+            ->add('section')
+            ->add('controltype')
+//            ->add('option_values')
+            ->add('created_at')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+//            ->add('id')
+            ->add('name')
+            ->add('description')
+            ->add('section')
+            ->add('controltype')
+//            ->add('option_values')
+            ->add('created_at')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+//            ->add('id')
+            ->add('name')
+            ->add('section')
+            ->add('description')
+            ->add('option_values')
+            ->add('created_at')
+        ;
+    }
+}

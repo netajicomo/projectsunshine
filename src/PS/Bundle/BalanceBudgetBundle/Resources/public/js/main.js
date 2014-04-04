8ead5019e3155432e73a4e6ff3a736938aaba664
pipe <pipe@como-HP-dx2480-MT-FN870PA.(none)>
2014-04-04 19:59:00 +0530

    new slider

--- src/PS/Bundle/BalanceBudgetBundle/Resources/public/js/main.js
+++ src/PS/Bundle/BalanceBudgetBundle/Resources/public/js/main.js
@@ -192,6 +192,7 @@ $(document).ready(function(){
                     
                 }
 	});
+        $(totalSlider).find( ".ui-slider-range" ).append("<div class='ui-slider-inner'><ul><li>Sustainable Level</li><li>Target</li><li>Unsustainable Level</li></ul></div>");
         var debtValue = (value/1000).toFixed(2);
         totalSlider.find( ".ui-slider-handle" ).append( "<span class='sliderValue'>$<em>"+debtValue+"</em> B </span>" );
         if(debtValue > 50)

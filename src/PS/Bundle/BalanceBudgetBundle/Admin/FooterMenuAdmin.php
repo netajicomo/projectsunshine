019aec74d597d421ea98baa627267d8a379f7445
Pushparaj <pushparaj@comogroupap.com>
2014-04-05 12:03:08 +0530

    header and footer

--- src/PS/Bundle/BalanceBudgetBundle/Admin/FooterMenuAdmin.php
+++ src/PS/Bundle/BalanceBudgetBundle/Admin/FooterMenuAdmin.php
@@ -8,8 +8,21 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
 
-class HeaderMenuAdmin extends Admin
+class FooterMenuAdmin extends Admin
 {
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('title')
+            ->add('url_type')
+            ->add('url')
+            ->add('target')
+        ;
+    }
 
     /**
      * @param ListMapper $listMapper
@@ -17,13 +30,10 @@ class HeaderMenuAdmin extends Admin
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            //->add('id')
+            ->add('id')
             ->add('title')
             ->add('url_type')
-
-           // ->add('lead')
-          //  ->add('description')
-            ->add('url',null, array('label' => 'URL'))
+            ->add('url')
             ->add('target')
             ->add('_action', 'actions', array(
                 'actions' => array(
@@ -41,14 +51,11 @@ class HeaderMenuAdmin extends Admin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-           // ->add('id')
+//            ->add('id')
             ->add('title')
             ->add('url_type','choice',array('required' => true,'label' => 'URL type','choices' => array('main' => 'Main','internal' => 'Internal','external' => 'External')))
             ->add('url')
             ->add('target',null,array('required' => false))
-            ->add('parent','entity',array('class' => 'PSBalanceBudgetBundle:HeaderMenu','property' => 'name', 'empty_value' => 'Select an Parent', 'required' =>false))
-            // ->add('is_active')
-           // ->add('created_at')
         ;
     }
 
@@ -64,22 +71,5 @@ class HeaderMenuAdmin extends Admin
             ->add('url')
             ->add('target')
         ;
-    }
-    
-    public function getTemplate($name)
-    {
-        switch ($name) {
-            case 'edit':
-               return 'PSBalanceBudgetBundle:Admin:edit.html.twig';
-                break;
-            default:
-                return parent::getTemplate($name);
-                break;
-        }
-    }
-    
-    protected function configureRoutes(\Sonata\AdminBundle\Route\RouteCollection $collection) {
-      $collection->add('getControlTypeProperties');
-    
     }
 }
--- src/PS/Bundle/BalanceBudgetBundle/Controller/DefaultController.php
+++ src/PS/Bundle/BalanceBudgetBundle/Controller/DefaultController.php
@@ -63,7 +63,7 @@ class DefaultController extends Controller
                 $session = $request->getSession();
                 $sessionId = $session->get('id');
                 try{
-                    $plannerPostCode_check = $em->getRepository('PSBalanceBudgetBundle:PlannerPostCode')->findOneBy(array('session_id'=>$sessionId));
+                        $plannerPostCode_check = $em->getRepository('PSBalanceBudgetBundle:PlannerPostCode')->findOneBy(array('session_id'=>$sessionId));
                 }
                 catch(\Exception $exception)
                 {
@@ -92,7 +92,20 @@ class DefaultController extends Controller
         $template = $this->getRequest()->get('template')?$this->getRequest()->get('template'):'PSBalanceBudgetBundle:Default:offline_submission.html.twig';
         return $this->render($template);
     }
-
+    public function dynamicHeaderAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $header_menu = $em->getRepository('PSBalanceBudgetBundle:HeaderMenu')->findBy(array('parent' =>  NULL));
+        $template = $this->getRequest()->get('template')?$this->getRequest()->get('template'):'PSBalanceBudgetBundle:Default:dynamic_header.html.twig';
+        return $this->render($template,array('header_menu' => $header_menu));
+    }
+    public function dynamicFooterAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $footer_menu = $em->getRepository('PSBalanceBudgetBundle:FooterMenu')->findAll();
+        $template = $this->getRequest()->get('template')?$this->getRequest()->get('template'):'PSBalanceBudgetBundle:Default:dynamic_footer.html.twig';
+        return $this->render($template,array('footer_menu' => $footer_menu));
+    }
 
 }
 
